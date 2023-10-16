f20d98f952a7e54017ea4cd8c2418db087075efa
Marcus <marcusfilipi.ventura@outlook.com>
2023-10-16 12:04:53 -0300

    aulaudemy

--- aula141/main.py
+++ aula141/main.py
@@ -1,1 +1,8 @@
-print("oi")
+from log import LogFileMixin, LogPrintMixin
+
+lp = LogPrintMixin()
+lp.log_error('qualquer coisa')
+lp.log_success('Que legal')
+lf = LogFileMixin()
+lf.log_error('qualquer coisa')
+lf.log_success('Que legal')
