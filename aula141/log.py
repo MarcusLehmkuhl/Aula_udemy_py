ad1c679f71082ad2a56b35cf91c593a3a9c9f1a5
Marcus <marcusfilipi.ventura@outlook.com>
2023-10-16 16:22:58 -0300

    aulaudemy

--- aula141/log.py
+++ aula141/log.py
@@ -8,7 +8,13 @@ LOG_FILE = Path(__file__).parent / 'log.txt'
 class Log:
     def _log(self, msg):
         raise NotImplementedError('Implemente o método log')
+    
+    def log_error(self, msg):
+        return self._log(f"Error: {msg}")
+
     def log_success(self, msg):
+         return self._log(f"Success: {msg}")
+        
 
 class LogFileMixin(Log):
     def _log(self, msg):
