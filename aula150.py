6bb3cfac51bc4c5a497ac913ec42ce0d0cc7acf0
MarcusLehmkuhl <marcusfilipi.ventura@outlook.com>
2023-10-24 18:28:57 -0300

    aulaudemy

--- aula150.py
+++ aula150.py
@@ -17,6 +17,6 @@ def my_open(caminho_arquivo, modo):
 
 with my_open('aula150.txt', 'w') as arquivo:
     arquivo.write('Linha 1\n')
-    arquivo.write('Linha 2\n', 123)
+    arquivo.write('Linha 2\n')
     arquivo.write('Linha 3\n')
     print('WITH', arquivo)
